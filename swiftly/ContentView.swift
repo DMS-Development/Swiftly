6d2e44eebf95be8dfad10a35b4ed70d0c21b2d53
David <davidshimenko@gmail.com>
2023-11-24 23:37:19 -0700

    Initial Commit

---
+++ swiftly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftly
+//
+//  Created by David Shimenko on 11/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftly/swiftlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftlyApp.swift
+//  swiftly
+//
+//  Created by David Shimenko on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
